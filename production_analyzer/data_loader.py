0ca07928a6339a2b789b9b46faad525dd0aff71f
James P. Imes <jamesimes@gmail.com>
2022-12-08 18:42:21 -0700

    refactor: spin off DataLoader class out of production_analyzer.py; add new HTMLLoader class

--- production_analyzer/data_loader.py
+++ production_analyzer/data_loader.py
@@ -1,7 +1,11 @@
 
+"""
+Helper classes for loading production data from various sources (.csv,
+.xlsx, existing ```DataFrame``` objects, and directly from websites).
+"""
+
 import os
 from pathlib import Path
-from datetime import datetime
 
 import pandas as pd
 import requests
@@ -14,7 +18,14 @@ class DataLoader:
 
     Note: All data sources should have the same headers, etc.
     """
-    def __init__(self, date_col: str, config: dict = None):
+    def __init__(
+            self,
+            date_col: str,
+            oil_prod_col: str = None,
+            gas_prod_col: str = None,
+            days_produced_col: str = None,
+            status_col: str = None,
+    ):
         """
         Load production data from one or more .csv or .xlsx files, or
         incorporate any number of existing ```DataFrame```.
@@ -25,9 +36,50 @@ class DataLoader:
          data sources that will be added.
         """
         self.date_col = date_col
+        self.oil_prod_col = oil_prod_col
+        self.gas_prod_col = gas_prod_col
+        self.days_produced_col = days_produced_col
+        self.status_col = status_col
         self.dfs = []
-        self.config = config
         self.files_saved = []
+
+    @classmethod
+    def from_config(cls, config):
+        """
+        Get a new ```DataLoader``` from a config dict.
+
+        :param config: A config dict, as loaded by ```config_loader```
+         module.
+        :return: A new ```DataLoader``` that incorporates the config
+         data.
+        """
+        relevant_fields = [
+            'date_col',
+            'oil_prod_col',
+            'gas_prod_col',
+            'days_produced_col',
+            'status_col',
+        ]
+        kw = {
+            param: val for param, val in config.items()
+            if param in relevant_fields
+        }
+        return cls(**kw)
+
+    @property
+    def configured_columns(self):
+        """
+        Get a list of column headers that have been configured for this
+        ```DataLoader```.
+        """
+        possible = [
+            self.date_col,
+            self.oil_prod_col,
+            self.gas_prod_col,
+            self.days_produced_col,
+            self.status_col,
+        ]
+        return [col for col in possible if col is not None]
 
     def add_csv(
             self,
@@ -211,11 +263,199 @@ class DataLoader:
             df[source_header] = source
         self.dfs.extend(dfs)
 
-    def output(self) -> pd.DataFrame:
+    def output(self, empty_as_error=False) -> pd.DataFrame:
         """
         Get the concatenated ```DataFrame``` for all data sources that
         have been loaded so far.
+        :param empty_as_error: (Optional) Specify ```True``` to raise
+         a ```ValueError``` if no data has been successfully loaded.
         :return: A ```DataFrame``` of all data sources, with no grouping
          and without filling any empty values.
         """
-        return pd.concat(self.dfs, ignore_index=True)
+        if self.dfs:
+            return pd.concat(self.dfs, ignore_index=True)
+        elif empty_as_error:
+            raise ValueError('No data loaded.')
+        else:
+            return pd.DataFrame(data=[], columns=self.configured_columns)
+
+
+class HTMLLoader:
+    """
+    A class to scrape production data from webpages that contain tabular
+    monthly production data in HTML.
+    """
+
+    def __init__(
+            self,
+            prod_url_template: str,
+            date_col: str,
+            oil_prod_col: str = None,
+            gas_prod_col: str = None,
+            days_produced_col: str = None,
+            status_col: str = None,
+            auth=None,
+    ):
+        """
+        A class to scrape production data from webpages that contain
+        tabular monthly production data in HTML.
+
+        The URL template should have formatting brackets
+        ```prod_url_template``` should be a string to the URL for the
+        production records, with formatting brackets for the portion
+        that will be filled in with meaningful data for each unique
+        well.
+
+        For example, Colorado uses a URL schema as follows:
+            ```"https://cogcc.state.co.us/cogisdb/Facility/Production?api_county_code={0}&api_seq_num={1}"```
+
+        ... which uses portions of the API number for a given well
+        (unique to each well) -- e.g., ```'05-001-12345'```.  In this,
+        ```'001'``` encodes the county, and ```'12345'``` is a unique
+        sequence for a specific well within that county.  These are
+        plugged into ```{0}``` and ```{1}``` within an f-string, to
+        generate the following URL:
+          ```"https://cogcc.state.co.us/cogisdb/Facility/Production?api_county_code=001&api_seq_num=12345"```
+
+        On the other hand, North Dakota uses a URL schema as follows:
+          ```https://www.dmr.nd.gov/oilgas/feeservices/getwellprod.asp?filenumber={0}```
+
+        ... where ```{0}``` would be filled with a unique, incrementing
+        5-digit number that gets assigned to wells by the State of North
+        Dakota itself.
+
+        :param prod_url_template: The template for the URL to the page
+         that will contain the data to scrape. (See above.)
+        :param date_col: The column header for the ```datetime``` that
+         represents its entire month (e.g., 1/1/2011 for January 2011).
+        :param oil_prod_col: (Optional) The header for oil production.
+        :param gas_prod_col: (Optional) The header for gas production.
+        :param days_produced_col: The column header for the number of
+         days that the well produced during this month.
+        :param status_col: (Optional) The header for the column
+         containing status codes.
+        :param auth: (Optional) If needed, provide the appropriate
+         ```requests.auth``` object for this state's website, such as:
+         ```requests.auth.HTTPBasicAuth('user_name', 'password')```.
+        """
+        self.prod_url_template = prod_url_template
+        self.date_col = date_col
+        self.oil_prod_col = oil_prod_col
+        self.gas_prod_col = gas_prod_col
+        self.days_produced_col = days_produced_col
+        self.status_col = status_col
+        self.auth = auth
+        self.dfs = []
+        self.files_saved = []
+
+    @classmethod
+    def from_config(cls, config: dict, auth=None):
+        """
+        Get a new ```HTMLLoader``` from a config dict.
+
+        :param config: A config dict, as loaded by ```config_loader```
+         module.
+        :param auth: (Optional) If needed, provide the appropriate
+         ```requests.auth``` object for this state, such as:
+         ```requests.auth.HTTPBasicAuth('user_name', 'password')```.
+        :return: A new ```HTMLLoader``` that incorporates the config
+         data.
+        """
+        return cls(**config, auth=auth)
+
+    @property
+    def configured_columns(self):
+        """
+        Get a list of column headers that have been configured for this
+        ```HTMLLoader```.
+        """
+        possible = [
+            self.date_col,
+            self.oil_prod_col,
+            self.gas_prod_col,
+            self.days_produced_col,
+            self.status_col,
+        ]
+        return [col for col in possible if col is not None]
+
+    def reset(self) -> None:
+        """
+        Reset the list of ```DataFrame``` objects in ```.dfs``` and the
+        list of filepaths in ```.files_saved``` to new, empty lists.
+        :return: None
+        """
+        self.dfs = []
+        self.files_saved = []
+
+    def get_html(self, url) -> str:
+        """
+        Get the HTML from the specified URL.
+
+        :param url: The webpage to scrape.
+        :return: The raw HTML text of the webpage.
+        """
+        r = requests.get(url=url, auth=self.auth)
+        return r.text
+
+    def save_html(self, txt, fp):
+        """
+        Save the HTML to the specified filename (if ```.save_html_dir```
+        has been configured).
+        """
+        fp = Path(fp)
+        os.makedirs(fp.parent, exist_ok=True)
+        with open(fp, 'w') as f:
+            f.write(txt)
+        self.files_saved.append(fp)
+        return None
+
+    def save_csv(self, df, fp) -> None:
+        """
+        Save the production ```DataFrame``` to .csv.
+        """
+        fp = Path(fp)
+        os.makedirs(fp.parent, exist_ok=True)
+        df.to_csv(fp)
+        self.files_saved.append(fp)
+        return None
+
+    def get_production_url(self, url_components):
+        """
+        Get the production URL for a specific well.
+
+        :param url_components: The components (i.e. a list of strings)
+         to plug into the url to get the correct URL.  For example,
+         the Colorado URL template takes two components, carved out of
+         the unique API code for each well: specifically, the county
+         code (3 digits) and a 5-digit sequence unique to each well.
+        :return:
+        """
+        if self.prod_url_template is None:
+            msg = 'URL not configured (may not be possible for this state).'
+            raise RuntimeError(msg)
+        return self.prod_url_template.format(*url_components)
+
+    def get_production_data_from_html(self, html):
+        """
+        Extract the production table(s) found in the provided HTML text
+        to a single ```DataFrame```. If not found return ```None```.
+
+        :param html: The raw text of the HTML from which to pull
+         production data table(s).
+        :return: The production ```DataFrame```, if found. If not found,
+         then ```None```.
+        """
+        matching_dfs = []
+        target_cols = set(self.configured_columns)
+        dfs = pd.read_html(html)
+        for df in dfs:
+            df_cols = set(df.columns)
+            if target_cols.issubset(df_cols):
+                matching_dfs.append(df)
+        if not matching_dfs:
+            return None
+        output = matching_dfs[0]
+        if len(matching_dfs) > 1:
+            output = pd.concat(matching_dfs)
+        self.dfs.append(output)
+        return output
